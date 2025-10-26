c9e6fdfdf65832823b925ff8b047dd12f939442f
Sergio <slamtor2707@g.educaand.es>
2025-10-26 22:03:21 +0100

    Progreso: ejercicio1-asignado valor a la variable

--- apartado2.1/ejercicio1/hola_variable.py
+++ apartado2.1/ejercicio1/hola_variable.py
@@ -1,9 +1,7 @@
-""" 
-1. Hola, variable
+"""
 • Objetivo: declarar variables y print.
 • Consigna: crea persona = "Pepe" y muestra “Hola, Pepe”. Comenta qué pasa si
 cambias el valor.
-• Referencia: variables y asignación (págs. 2–3). 
+• Referencia: variables y asignación (págs. 2–3).
 """
 persona = "Pepe"
-
