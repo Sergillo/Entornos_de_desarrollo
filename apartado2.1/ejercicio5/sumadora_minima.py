33e790b1a3d288988247954c00fe7ebf7ec6c37e
Sergio <slamtor2707@g.educaand.es>
2025-10-26 22:44:22 +0100

    Progreso: ejercicio5-sin el int en el input

---
+++ apartado2.1/ejercicio5/sumadora_minima.py
@@ -0,0 +1,11 @@
+"""
+Sumadora mínima
+• Objetivo: operar con números vs cadenas.
+• Consigna: pide dos números; demuestra qué ocurre si sumas como cadenas y luego corrígelo
+con int().
+• Referencia: diferencia entre "10" y 10 (págs. 5 y 10–11).
+"""
+num1 = input("Introduce un primer numero: ")
+num2 = input("Introduce un segundo numero: ")
+suma = num1 + num2
+print("La suma es de:", suma)
