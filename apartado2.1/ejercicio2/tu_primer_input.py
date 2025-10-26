fe017f441d0c32b052e6941b65d785cb88a88c65
Sergio <slamtor2707@g.educaand.es>
2025-10-26 22:10:28 +0100

    Progreso: ejercicio2-variable asignada con un input

---
+++ apartado2.1/ejercicio2/tu_primer_input.py
@@ -0,0 +1,7 @@
+""""
+2. Tu primer input
+• Objetivo: leer texto del usuario.
+• Consigna: pide el nombre y muestra Hola <nombre>.
+• Referencia: input y texto de prompt (pág. 4).
+"""
+nombre = input ("Introduzca su nombre")
