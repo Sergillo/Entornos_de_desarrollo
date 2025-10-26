f0150d22f449bbfe38a627bfc2dd10f9d9a90626
Sergio <slamtor2707@g.educaand.es>
2025-10-26 23:31:35 +0100

    Progreso: ejercicio8- mitad de las opciones

---
+++ apartado2.1/ejercicio8/limpia_y_formatea.py
@@ -0,0 +1,10 @@
+"""
+8. Limpia y formatea
+• Objetivo: strip, lower, upper.
+• Consigna: lee una frase con espacios delante y detrás; muestra: original, con strip(), en
+minúsculas y en mayúsculas.
+• Referencia: replace, strip, lower/upper
+"""
+frase = input("ingresa una frase con espacios al principio y al final")
+print("Frase original:", frase)
+print("Frase con sin espacio al principio y al final:", frase.strip())
