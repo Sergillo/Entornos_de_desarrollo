6e564e88a68d930c68da5343b593f5c1515701b0
Sergio <slamtor2707@g.educaand.es>
2025-10-26 22:21:56 +0100

    Progreso: ejercicio3- variable valor numerico pedida

---
+++ apartado2.1/ejercicio3/edad_correcta.py
@@ -0,0 +1,7 @@
+"""
+3. Edad correcta
+• Objetivo: convertir tipos.
+• Consigna: pide año de nacimiento, conviértelo con int() y calcula edad.
+• Referencia: conversión de tipos y int()
+"""
+año_nacimiento = (int(input("Introduce el año en el que naciste: ")))
