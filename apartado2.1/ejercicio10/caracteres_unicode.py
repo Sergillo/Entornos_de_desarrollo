b81f5aaf335aea56c8300aa7f33da2c1e292d37e
Sergio <slamtor2707@g.educaand.es>
2025-10-26 23:49:32 +0100

    Progreso: ejercicio10-primero hecho

---
+++ apartado2.1/ejercicio10/caracteres_unicode.py
@@ -0,0 +1,7 @@
+"""
+10.Caracteres Unicode
+• Objetivo: chr() y curiosidad por Unicode.
+• Consigna: imprime 3 emojis usando sus códigos.
+• Referencia: ejemplos chr(0x1F415)... (pág. 13).
+"""
+print(chr(0x1F600))
