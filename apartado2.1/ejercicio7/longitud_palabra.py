501c8043538ecaaaafe66d39fc66529481b2aa70
Sergio <slamtor2707@g.educaand.es>
2025-10-26 23:18:26 +0100

    Progreso: ejercicio7-calculamos longitud

---
+++ apartado2.1/ejercicio7/longitud_palabra.py
@@ -0,0 +1,8 @@
+"""
+7. Longitud de una palabra
+• Objetivo: len() y print.
+• Consigna: pide una palabra y muestra su longitud.
+• Referencia: len (pág. 11).
+"""
+palabra = input("Introduce una palabra para mostrar su longitud: ")
+longitud = len(palabra)
