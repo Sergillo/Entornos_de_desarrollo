3fe489fc776bc9b4b469472989e97b95e092fe20
Sergio <slamtor2707@g.educaand.es>
2025-10-26 23:44:18 +0100

    Progreso: ejercicio9- primera parte del f-strings

---
+++ apartado2.1/ejercicio9/mi_primera_fstring.py
@@ -0,0 +1,10 @@
+"""
+9. Mi primera f-string
+• Objetivo: f-strings.
+• Consigna: con nombre y edad, imprime: Pepe tiene 20 años.
+• Extensión: muestra alineación de decimales :6.3f.
+• Referencia: f-strings y formato (págs. 12–13).
+"""
+nombre = "Pepe"
+edad = 20
+print(f"{nombre} tiene {edad} años")
