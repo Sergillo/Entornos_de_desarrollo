c306c9c93bb7ce14e02a4f48a7c90efc86efecd4
Sergio <slamtor2707@g.educaand.es>
2025-10-26 21:02:35 +0100

    Progreso: variable igualada a Pepe

---
+++ apartado2.1/ejercicio1/hola_variable.py
@@ -0,0 +1,9 @@
+""" 
+1. Hola, variable
+• Objetivo: declarar variables y print.
+• Consigna: crea persona = "Pepe" y muestra “Hola, Pepe”. Comenta qué pasa si
+cambias el valor.
+• Referencia: variables y asignación (págs. 2–3). 
+"""
+persona = "Pepe"
+
