b9b0c8d6d151d26c2d3010678de7e0b70210f968
Sergio <slamtor2707@g.educaand.es>
2025-10-26 23:06:51 +0100

    Progreso: ejercicio6- mitad de las operaciones

---
+++ apartado2.1/ejercicio6/operadores_en_accion.py
@@ -0,0 +1,11 @@
+"""
+Operadores en acción
+• Objetivo: practicar + - * / // % ** y +=.
+• Consigna: con dos enteros a y b imprime todas esas operaciones.
+• Referencia: tabla de operadores (pág. 10).
+"""
+a = int(input("Introduce el primer numero: "))
+b = int(input("Introduce el segundo numero: "))
+print("La suma de", a, "y", b, "es", a + b)
+print("La resta de", a, "y", b, "es", a - b)
+print("La multiplicación de", a, "y", b, "es", a * b)
