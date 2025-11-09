9146d907db7d1f59fed466c9071c051cc63eb5d4
Sergio <slamtor2707@g.educaand.es>
2025-11-09 18:41:35 +0100

    Control de mensajes de errores Ejercicio5

---
+++ Leccion2/ejercicio5/sumadora_minima.py
@@ -0,0 +1,14 @@
+"""
+Sumadora mínima
+• Objetivo: operar con números vs cadenas.
+• Consigna: pide dos números; demuestra qué ocurre si sumas como cadenas y luego corrígelo
+con int().
+• Referencia: diferencia entre "10" y 10 (págs. 5 y 10–11).
+"""
+try:
+    num1 = int(input("Introduce un primer numero: "))
+    num2 = int(input("Introduce un segundo numero: "))
+    suma = num1 + num2
+    print("La suma es de:", suma)
+except ValueError:
+    print("Por favor, introduce valores numericos")
