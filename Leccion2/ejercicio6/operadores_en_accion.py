0d0ba70af2411ed4694326cbe82cb0de40c3be07
Sergio <slamtor2707@g.educaand.es>
2025-11-09 18:46:26 +0100

    Control de mensajes de errores Ejercicio6

---
+++ Leccion2/ejercicio6/operadores_en_accion.py
@@ -0,0 +1,18 @@
+"""
+Operadores en acción
+• Objetivo: practicar + - * / // % ** y +=.
+• Consigna: con dos enteros a y b imprime todas esas operaciones.
+• Referencia: tabla de operadores (pág. 10).
+    """
+try:
+    a = int(input("Introduce el primer numero: "))
+    b = int(input("Introduce el segundo numero: "))
+    print("La suma de", a, "y", b, "es", a + b)
+    print("La resta de", a, "y", b, "es", a - b)
+    print("La multiplicación de", a, "y", b, "es", a * b)
+    print("La división de", a, "y", b, "es", a / b)
+    print("La división entera de", a, "y", b, "es", a // b)
+    print("El resto de la división de", a, "y", b, "es", a % b)
+    print("La potencia de", a, "y", b, "es", a ** b)
+except ValueError:
+    print("Por favor, introduce valores numericos")
